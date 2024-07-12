553c0aaa7e8b4c3ab6d058de7c2da9732ea5f26e
Nns_eyejang <78232334+net225@users.noreply.github.com>
2024-07-13 05:04:07 +0700

    round1

--- my-truffle-project/client/cubs-web/src/app/Profile/page.tsx
+++ my-truffle-project/client/cubs-web/src/app/Profile/page.tsx
@@ -1,37 +1,192 @@
 "use client"
 import Sidebar from '@/app/component/Slidebar'
-
+import Image from 'next/image'
+import profileMan from '/public/logoMan.svg'
+import EditPic from '/public/Edit.svg'
 
 const Profile = () => {
     return (
       <div className="flex flex-col md:flex-row">
         <Sidebar/>
         <div className="flex-1 p-6">
-          <h1 className="text-2xl font-bold mb-4">My Profile</h1>
-          <div className="bg-white p-4 rounded-lg shadow-md">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <ProfileField label="Name" value="Someone" />
-              <ProfileField label="Surname" value="Helpme" />
-              <ProfileField label="Nickname" value="Eiei" />
-              <ProfileField label="Age" value="18" />
-              <ProfileField label="Birthday" value="29/02/2004" />
-              <ProfileField label="Weight" value="56 kg" />
-              <ProfileField label="Height" value="183 cm" />
-              <ProfileField label="Religion" value="Buddhism" />
-              <ProfileField label="Nationality" value="Thai" />
-              <ProfileField label="Blood type" value="AB" />
-              <ProfileField label="Address" value="xxxxxxxxxxxxxxx" />
-              <ProfileField label="School" value="xxxxx" />
-              <ProfileField label="Field of study" value="Science-Math" />
-              <ProfileField label="Hobbies" value="Listening to music, sleeping" />
-              <ProfileField label="Email" value="xxx@gmail.com" />
-              <ProfileField label="Phone number" value="08x-xxx-xxx-x" />
-              <ProfileField label="Contact" value="instagram facebook" />
-              <ProfileField label="Father" value="................" />
-              <ProfileField label="Mother" value="................" />
+          <h1 className="text-2xl font-sans font-bold mb-4 ml-4 mt-10">My Profile</h1>
+          
+          {/* Profile */}
+          <div className="border border-4 w-full rounded-3xl grid">
+            <div className='flex'>
+              <Image
+                className="m-3 ml-7"
+                src={profileMan}
+                alt="Picture of the author"
+                width={100} 
+              />
+              <div className='p-5 ml-2 mt-4'>
+                <div className='mb-2'>
+                  <h1 className='font-bold'>User Name : 
+                  <span className='font-medium text-gray-500'> Someone Helpme</span>
+                  </h1>
+                </div>
+
+                <h1 className='font-bold'>Describe :  
+                <span className='font-medium text-gray-500'>  Science-Mathematics</span>
+                </h1>
+              </div>
             </div>
-            <button className="mt-4 py-2 px-4 bg-purple-500 text-white rounded">Confirm</button>
           </div>
+
+
+          {/*Personal-Details*/}
+          <div className="border border-4 w-full rounded-3xl grid mt-3">
+            <div className='flex'>
+              <h1 className='text-xl font-sans font-bold mb-3 pt-4 pl-6'>Personal Details</h1>
+              <Image
+                className="relative left-11"
+                src={EditPic}
+                alt="Picture of the author"
+                width={60} 
+              />
+            </div>
+
+            <div className="flex pl-6">
+              {/* detail-column1 */}
+              <div>
+                <div className='mb-2'>
+                  <h1 className='font-bold'>First Name : 
+                  <span className='font-medium text-gray-500'> Someone</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Date of Birth : 
+                  <span className='font-medium text-gray-500'> 29/02/2004</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Religion : 
+                  <span className='font-medium text-gray-500'> Buddhism</span>
+                  </h1>
+                </div>
+
+                <div className='mb-6'>
+                  <h1 className='font-bold'>Hobbies : 
+                  <span className='font-medium text-gray-500'> Listening to music, sleeping</span>
+                  </h1>
+                </div>
+              </div>
+
+              {/* detail-column2 */}
+              <div className='pl-24'>
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Last Name : 
+                  <span className='font-medium text-gray-500'> Helpme</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Age : 
+                  <span className='font-medium text-gray-500'> 18</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Nationality : 
+                  <span className='font-medium text-gray-500'> Thai</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Weight : 
+                  <span className='font-medium text-gray-500'> 70 kg</span>
+                  </h1>
+                </div>
+              </div>
+
+              {/* detail-column3 */}
+              <div className='pl-32'>
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Nickname : 
+                  <span className='font-medium text-gray-500'> Eiei</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Gender : 
+                  <span className='font-medium text-gray-500'> Male</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Blood type : 
+                  <span className='font-medium text-gray-500'> AB</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Height : 
+                  <span className='font-medium text-gray-500'> 183 cm</span>
+                  </h1>
+                </div>
+              </div>  
+            </div>
+          </div>
+
+          {/*Address*/}
+          <div className="border border-4 w-full rounded-3xl grid mt-3">
+            <h1 className='text-xl font-sans font-bold mb-3 pt-4 pl-6'>Address</h1>
+            <div className="flex pl-6">
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Country : 
+                  <span className='font-medium text-gray-500'> xxxxx</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2 pl-28'>
+                  <h1 className='font-bold'>City : 
+                  <span className='font-medium text-gray-500'> xxxxx</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2 pl-28'>
+                  <h1 className='font-bold'>Province : 
+                  <span className='font-medium text-gray-500'> xxxxx</span>
+                  </h1>
+                </div>
+
+                <div className='mb-5 pl-28'>
+                  <h1 className='font-bold'>Zip Code : 
+                  <span className='font-medium text-gray-500'> xxxxx</span>
+                  </h1>
+                </div>
+            </div>
+          </div>
+
+          {/*Contact-Details*/}
+          <div className="border border-4 w-full rounded-3xl grid mt-3">
+            <h1 className='text-xl font-sans font-bold mb-3 pt-4 pl-6'>Contact Details</h1>
+            <div className="flex pl-6">
+
+                <div className='mb-2'>
+                  <h1 className='font-bold'>Email : 
+                  <span className='font-medium text-gray-500'> xxx@gmail.com</span>
+                  </h1>
+                </div>
+
+                <div className='mb-2 pl-28'>
+                  <h1 className='font-bold'>Phone number : 
+                  <span className='font-medium text-gray-500'> xxxxx</span>
+                  </h1>
+                </div>
+
+                <div className='mb-5 pl-28'>
+                  <h1 className='font-bold'>Facebook : 
+                  <span className='font-medium text-gray-500'> xxxxx</span>
+                  </h1>
+                </div>
+            </div>
+          </div>
+
         </div>
       </div>
     );
--- my-truffle-project/client/cubs-web/src/app/component/Slidebar.tsx
+++ my-truffle-project/client/cubs-web/src/app/component/Slidebar.tsx
@@ -16,7 +16,7 @@ const Sidebar = () => {
         <h1 className="m-2 text-lg font-bold">BLOCKTECTIVE</h1>
       </div>
       <div className="flex flex-col items-center mb-6">
-        <h1 className="m-2 pb-2 text-xl font-semibold font-mono">My Account</h1>
+        <h1 className="m-2 pb-2 text-2xl font-semibold font-mono">My Account</h1>
           <Image
             className='pb-3'
             src={profileMan}
@@ -25,25 +25,25 @@ const Sidebar = () => {
             height={60} 
         />
         <div className="ml-4">
-          <p className="font-bold">Someone   Helpme</p>
-          <p className="text-sm">Science-Mathematics</p>
+          <p className="text-center text-lg tracking-wide font-bold">Someone Helpme</p>
+          <p className="text-center text-stone-500 tracking-wide text-sm">Science-Mathematics</p>
         </div>
       </div>
       <nav>
         <Link href="/profile">
-          <p className="block py-2 px-4 rounded bg-purple-300">My Profile</p>
+          <p className="text-center block py-2 px-4 rounded bg-purple-300 text-white font-semibold">My Profile</p>
         </Link>
         <Link href="/portfolio">
-          <p className="block py-2 px-4 rounded hover:bg-purple-300">My Portfolio</p>
+          <p className="text-center block py-2 px-4 rounded hover:bg-purple-300">My Portfolio</p>
         </Link>
         <Link href="/favorites">
-          <p className="block py-2 px-4 rounded hover:bg-purple-300">Favorites</p>
+          <p className="text-center block py-2 px-4 rounded hover:bg-purple-300">Favorites</p>
         </Link>
         <Link href="/notifications">
-          <p className="block py-2 px-4 rounded hover:bg-purple-300">Notifications</p>
+          <p className="text-center block py-2 px-4 rounded hover:bg-purple-300">Notifications</p>
         </Link>
         <Link href="/settings">
-          <p className="block py-2 px-4 rounded hover:bg-purple-300">Settings</p>
+          <p className="text-center block py-2 px-4 rounded hover:bg-purple-300">Settings</p>
         </Link>
       </nav>
     </div>
